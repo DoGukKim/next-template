94098f4523d3b96425f598ad278a861642bf5936
DoGukKim <dgkim3811@naver.com>
2023-05-03 15:50:26 +0900

    feat: Grid 컴포넌트 생성

---
+++ src/components/Layout/Grid.tsx
@@ -0,0 +1,72 @@
+import { CSSProperties, ElementType, forwardRef } from 'react'
+import { css, cx } from '@emotion/css'
+
+import {
+  PolymorphicComponentPropsWithRef,
+  PolymorphicRef,
+} from 'shared/types/polymorphic'
+import { CombineType } from 'shared/types/util'
+import { LayoutElements } from './type'
+
+import { DEFAULT_ELEMENT } from './constant'
+
+type GridStyleProps = {
+  templateRows: CSSProperties['gridTemplateRows']
+  templateColumns: CSSProperties['gridTemplateColumns']
+  autoRows: CSSProperties['gridAutoRows']
+  autoColumns: CSSProperties['gridAutoColumns']
+  autoFlow: CSSProperties['gridAutoFlow']
+  gap: CSSProperties['gap']
+}
+
+type GridProps<E extends ElementType> = CombineType<
+  PolymorphicComponentPropsWithRef<E>,
+  Partial<GridStyleProps>
+>
+
+const Grid = forwardRef(
+  <E extends ElementType>(
+    {
+      as,
+      autoRows,
+      autoColumns,
+      autoFlow,
+      className,
+      children,
+      gap,
+      templateRows,
+      templateColumns,
+      ...props
+    }: GridProps<E | typeof DEFAULT_ELEMENT>,
+    forwardedRef: PolymorphicRef<E>
+  ) => {
+    const Element = as || DEFAULT_ELEMENT
+
+    return (
+      <Element
+        className={cx(
+          css`
+            display: grid;
+            grid-template-rows: ${templateRows};
+            grid-template-columns: ${templateColumns};
+            grid-auto-rows: ${autoRows};
+            grid-auto-columns: ${autoColumns};
+            grid-auto-flow: ${autoFlow};
+            gap: ${gap};
+          `,
+          className
+        )}
+        ref={forwardedRef}
+        {...props}
+      >
+        {children}
+      </Element>
+    )
+  }
+)
+
+Grid.displayName = 'Grid'
+
+export default Grid as <E extends LayoutElements>(
+  props: GridProps<E> & { ref?: PolymorphicRef<E> }
+) => ReturnType<typeof Grid>
